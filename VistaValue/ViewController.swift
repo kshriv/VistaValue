b9df76c2ccfb09a5e1c1c028eae95f6cfc8bc437
Komal <komal.shri1999@gmail.com>
2020-05-11 19:18:21 -0400

    background

--- VistaValue/ViewController.swift
+++ VistaValue/ViewController.swift
@@ -14,9 +14,23 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        assignbackground()
         for _ in 0...10 {
             setupCoin()
         }
+    }
+    
+    func assignbackground(){
+        let background = UIImage(named: "gradient")
+
+        var imageView : UIImageView!
+        imageView = UIImageView(frame: view.bounds)
+        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+        imageView.clipsToBounds = true
+        imageView.image = background
+        imageView.center = view.center
+        view.addSubview(imageView)
+        self.view.sendSubviewToBack(imageView)
     }
 
     func setupCoin() {
