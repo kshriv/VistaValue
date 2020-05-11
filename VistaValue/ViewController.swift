2cfb0afdbf7231e00e700d14e27831fe47717865
Komal <komal.shri1999@gmail.com>
2020-05-11 00:52:57 -0400

    og

--- VistaValue/ViewController.swift
+++ VistaValue/ViewController.swift
@@ -9,12 +9,73 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet var mainView: UIView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        for _ in 0...10 {
+            setupCoin()
+        }
     }
 
+    func setupCoin() {
+        //MARK: -first
+        var image = UIImageView()
+        image.tintColor = UIColor.blue
+        image.backgroundColor = UIColor.blue
+        image.frame = CGRect(x: 0.5 * (self.mainView.frame.width / 10) + (0 * (self.mainView.frame.width / 10)), y: -200, width: (self.mainView.frame.width / 10), height: (self.mainView.frame.width / 10))
+        self.view.addSubview(image)
+        
+        UIView.animate(withDuration: 5, delay: 0, options: [.repeat], animations: {
+            image.frame = CGRect(x: 0.5 * (self.view.frame.width / 10) + (0 * (self.view.frame.width / 10)), y: self.view.frame.midY * 2.2, width: (self.view.frame.width / 10), height: (self.view.frame.width / 10))
+        }, completion: nil)
+        
+        //MARK: -second
+        image = UIImageView()
+        image.tintColor = UIColor.blue
+        image.backgroundColor = UIColor.blue
+        image.frame = CGRect(x: 0.5 * (self.mainView.frame.width / 10) + (2 * (self.mainView.frame.width / 10)), y: self.mainView.frame.height + 200, width: (self.mainView.frame.width / 10), height: (self.mainView.frame.width / 10))
+        self.view.addSubview(image)
+        
+        UIView.animate(withDuration: 5, delay: 0, options: [.repeat], animations: {
+            image.frame = CGRect(x:  0.5 * (self.mainView.frame.width / 10) + (2 * (self.mainView.frame.width / 10)), y: -200, width: (self.mainView.frame.width / 10), height: (self.mainView.frame.width / 10))
+        }, completion: nil)
+        
+        //MARK: -third
+        image = UIImageView()
+        image.tintColor = UIColor.blue
+        image.backgroundColor = UIColor.blue
+        image.frame = CGRect(x: 0.5 * (self.mainView.frame.width / 10) + (4 * (self.mainView.frame.width / 10)), y: -200, width: (self.mainView.frame.width / 10), height: (self.mainView.frame.width / 10))
+        self.mainView.addSubview(image)
+
+        UIView.animate(withDuration: 5, delay: 0, options: [.repeat], animations: {
+            image.frame = CGRect(x:  0.5 * (self.view.frame.width / 10) + (4 * (self.view.frame.width / 10)), y: self.view.frame.midY * 2.2, width: (self.view.frame.width / 10), height: (self.view.frame.width / 10))
+        }, completion: nil)
+        
+        //MARK: -fourth
+       image = UIImageView()
+       image.tintColor = UIColor.blue
+       image.backgroundColor = UIColor.blue
+        image.frame = CGRect(x: 0.5 * (self.mainView.frame.width / 10) + (6 * (self.mainView.frame.width / 10)), y: self.mainView.frame.height + 200, width: (self.mainView.frame.width / 10), height: (self.mainView.frame.width / 10))
+       self.view.addSubview(image)
+       
+       UIView.animate(withDuration: 5, delay: 0, options: [.repeat], animations: {
+           image.frame = CGRect(x:  0.5 * (self.view.frame.width / 10) + (6 * (self.view.frame.width / 10)), y: -200, width: (self.view.frame.width / 10), height: (self.view.frame.width / 10))
+       }, completion: nil)
+        
+        //MARK: -fifth
+        image = UIImageView()
+        image.tintColor = UIColor.blue
+        image.backgroundColor = UIColor.blue
+        image.frame = CGRect(x: 0.5 * (self.mainView.frame.width / 10) + (8 * (self.mainView.frame.width / 10)), y: -200, width: (self.mainView.frame.width / 10), height: (self.mainView.frame.width / 10))
+        self.view.addSubview(image)
+
+        UIView.animate(withDuration: 5, delay: 0, options: [.repeat], animations: {
+            image.frame = CGRect(x:  0.5 * (self.mainView.frame.width / 10) + (8 * (self.mainView.frame.width / 10)), y: self.mainView.frame.midY * 2.2, width: (self.mainView.frame.width / 10), height: (self.mainView.frame.width / 10))
+        }, completion: nil)
+    
+    }
 
 }
 
